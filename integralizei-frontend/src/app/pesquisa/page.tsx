016e470ab54a2fd82c7e1a131aed1bc7f047780f
carolinabecker <carolsmbecker@gmail.com>
2025-10-25 15:33:49 -0300

    adiciona barra de pesquisa

--- integralizei-frontend/src/app/pesquisa/page.tsx
+++ integralizei-frontend/src/app/pesquisa/page.tsx
@@ -2,8 +2,7 @@
 
 import { useState } from "react";
 import Navbar2 from "../../components/Navbar2/Navbar2";
-import styles from "./pesquisa.module.css";
-
+import styles from "./pesquisa.module.css"; 
 
 export default function Page() {
   const [search, setSearch] = useState("");
@@ -12,7 +11,9 @@ export default function Page() {
   const [disciplinas, setDisciplinas] = useState<any[]>([]);
   const [error, setError] = useState("");
 
-  async function handleSearch() {
+  
+  async function handleSearch(e: React.FormEvent<HTMLFormElement>) {
+    if (e) e.preventDefault(); // Impede o reload
     
     try {
       setError("");
@@ -43,51 +44,67 @@ export default function Page() {
     }
   }
 
- return (
-    
+  return (
     <>
       <Navbar2 />
-        
+      
       <div className={styles.pageContainer}>
         
         <section className={styles.headerSection}>
           <h1 className={styles.title}>Pesquisa</h1>
           <p className={styles.subtitle}>
-            Simule sua integralização e veja como ela estara no proximo semestre
+            Simule sua integralização e veja como ela estará no proximo semestre
           </p>
         </section>
         
-        <div>
+        
+        <form className={styles.searchForm} onSubmit={handleSearch}>
           
-          <input
-            type="text"
-            placeholder="Código ou nome da disciplina"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            style={{ marginRight: "10px" }}
-          />
-          <input
-            type="number"
-            placeholder="Ano"
-            value={year}
-            onChange={(e) => setYear(parseInt(e.target.value))}
-            style={{ marginRight: "10px", width: "80px" }}
-          />
-          <input
-            type="number"
-            placeholder="Período"
-            value={period}
-            onChange={(e) => setPeriod(parseInt(e.target.value))}
-            style={{ marginRight: "10px", width: "80px" }}
-          />
-          <button onClick={handleSearch}>Buscar</button>
-        </div>
+          
+          <div className={styles.searchContainer}>
+            <input
+              type="text"
+              className={styles.searchInput}
+              placeholder="Código ou nome da disciplina"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button type="submit" className={styles.searchButton}>
+              
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+              </svg>
+            </button>
+          </div>
 
+          <div className={styles.optionsContainer}>
+            <div className={styles.inputGroup}>
+              <label htmlFor="year-input">Ano</label>
+              <input
+                id="year-input"
+                type="number"
+                className={styles.numberInput}
+                value={year}
+                onChange={(e) => setYear(parseInt(e.target.value))}
+              />
+            </div>
+            <div className={styles.inputGroup}>
+              <label htmlFor="period-input">Período</label>
+              <input
+                id="period-input"
+                type="number"
+                className={styles.numberInput}
+                value={period}
+                onChange={(e) => setPeriod(parseInt(e.target.value))}
+              />
+            </div>
+          </div>
+        </form>
+      
         {error && <p style={{ color: "red" }}>{error}</p>}
 
         {/* resultados */}
         <div style={{ marginTop: "20px" }}>
-          
           {disciplinas.length === 0 ? (
             <p>Nenhuma disciplina encontrada.</p>
           ) : (
