2290d6fb14d57d2eb6bedc3d74b67d49a7e89710
menali17 <enzomenali@gmail.com>
2025-11-28 20:50:20 -0300

    modified:   integralizei-backend/formulario-flask/pytest.ini 	modified:   integralizei-backend/formulario-flask/tests/test_exemplo.py 	modified:   integralizei-backend/pyproject.toml

--- integralizei-backend/formulario-flask/tests/test_exemplo.py
+++ integralizei-backend/formulario-flask/tests/test_exemplo.py
@@ -1,2 +1,4 @@
-def test_ambiente_configurado_corretamente():
-    assert 1 == 1
+def test_exemplo_usa_fixture(db_path):
+    # Se esse teste rodar, o conftest.py foi carregado
+    # e a fixture db_path existe.
+    assert db_path.endswith(".db")
